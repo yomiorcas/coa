942eb377527188ee2e0567253c5fab895ba6b0b5
Yomi Aderibigbe <yomiaderibigbe@gmail.com>
2022-07-26 12:07:22 +0100

    Updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -185,9 +185,7 @@
         });
     });
 
-	// Switch Btn
-	$('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-}(jQuery));
+	}(jQuery));
 
 
 // function to set a given theme/color-scheme
